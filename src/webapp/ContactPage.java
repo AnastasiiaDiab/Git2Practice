e0492d7971758c17ee91413dc72f9063be22e5be
lyaas <lyaastud@gmail.com>
2022-12-22 14:23:26 -0500

    Contact page created

---
+++ src/webapp/ContactPage.java
@@ -0,0 +1,14 @@
+package webapp;
+
+public class ContactPage {
+
+    //facebook
+    //twitter
+    //instagram
+    public static void main(String[] args) {
+
+        System.out.println("Contact page");
+
+        System.out.println("Please look below for the info");
+    }
+}
