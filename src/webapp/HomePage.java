224c868e4b84ecf43ce0b4d9156ef0eb349ab315
lyaas <lyaastud@gmail.com>
2022-12-22 14:05:55 -0500

    Home page setup

---
+++ src/webapp/HomePage.java
@@ -0,0 +1,14 @@
+package webapp;
+
+public class HomePage {
+
+    //Setting up homepage
+
+    // menu
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to Home Page!");
+
+    }
+}
