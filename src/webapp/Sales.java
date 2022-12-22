eb9b86d76fe4d854f7a834b6b23d9305b5630dbe
lyaas <lyaastud@gmail.com>
2022-12-22 15:16:08 -0500

    Sales created

---
+++ src/webapp/Sales.java
@@ -0,0 +1,12 @@
+package webapp;
+
+public class Sales {
+
+    public static void main(String[] args) {
+
+        System.out.println("15% OFF all blue tags!");
+
+        System.out.println("50% OFF all red tags!");
+
+    }
+}
