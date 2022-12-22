acd9a502fe2d5c9624633d4c7ba6a8881e854392
lyaas <lyaastud@gmail.com>
2022-12-22 14:07:38 -0500

    Home page update

--- src/webapp/HomePage.java
+++ src/webapp/HomePage.java
@@ -10,5 +10,9 @@ public class HomePage {
 
         System.out.println("Welcome to Home Page!");
 
+        System.out.println("Please Explore our website");
+
+        System.out.println("You can leave a feedback in the window below");
+
     }
 }
